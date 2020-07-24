58ad441ee89e99a5382a5953ba0061baecc3784c
Sandra Lee <sanlee@Sandras-Air.attlocal.net>
2020-07-24 10:35:30 -0400

    refactored shuffleArray

--- src/shuffleArray.java
+++ src/shuffleArray.java
@@ -6,17 +6,22 @@ Leetcode: Shuffle array - https://leetcode.com/problems/shuffle-an-array/solutio
 Slightly different answer from the leetcode problem. Just took a snippet of the randomnizer.
  */
 public class shuffleArray {
-public static void main(String args[]) {
-    int[] nums = {1,2,3};
-    Random random = new Random();
-    int[] arrayCopy = Arrays.copyOf(nums, nums.length);
+    public static void main(String args[]) {
+        Random random = new Random();
+        int[] nums = {1,2,3,4,5,6,7};
+        int[] results = Arrays.copyOf(nums, nums.length);
+        int j = 0;
+        while(j<3){
+        for(int i =0; i<nums.length;i++){
+            int temp = results[i];
+            // random number from 0-7
+            int position = random.nextInt(nums.length);
+            results[i] = results[position];
+            results[position] = temp;
+        }
 
-    for(int i =0;i<nums.length;i++){
-        int position = random.nextInt(nums.length);
-        int temp = arrayCopy[i];
-        arrayCopy[i] = arrayCopy[position];
-        arrayCopy[position] = temp;
+        System.out.println(Arrays.toString(results));
+        j++;
+        }
     }
-    System.out.println(Arrays.toString(arrayCopy));
-}
 }
